5537f174694f9a0cc5ce84d4fde5a7750f4bb743
rajithkonara <rajithkonara@gmail.com>
2021-01-24 00:34:40 +0530

    added sql script and configuration changes

---
+++ src/main/resources/script.sql
@@ -0,0 +1,8 @@
+INSERT INTO public.products(
+	id, adding_percentage, carton_price, created_at, discount, discount_margin, image_url, is_deleted, items_per_carton, name, updated_at)
+	VALUES ('pid-1feeb944-dd0b-49cf-be8f-60db1925eef0', 30, 175.00, current_timestamp, 10, 3, 'http://test.png', false, 20, 'Penguin ears', current_timestamp);
+
+INSERT INTO public.products(
+	id, adding_percentage, carton_price, created_at, discount, discount_margin, image_url, is_deleted, items_per_carton, name, updated_at)
+	VALUES ('pid-fbd7be8b-15e8-4d85-ad50-35bdba139f44', 30, 825.00, current_timestamp, 10, 3, 'http://test.png', false, 5, 'Horseshoe', current_timestamp);
+
